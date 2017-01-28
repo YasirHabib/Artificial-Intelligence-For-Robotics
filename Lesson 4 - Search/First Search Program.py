e739080f44ef9cd7d5ed7a20db735a722bbe3201
papillon1 <yasirhabib11@gmail.com>
2017-01-28 22:04:25 +0000

    Finished

--- Lesson 4 - Search/First Search Program.py
+++ Lesson 4 - Search/First Search Program.py
@@ -36,6 +36,7 @@ def search(grid,init,goal,cost):
     # insert code here
     # ----------------------------------------
     destination = True
+    path = True
     
     state = [[0 for row in range(len(grid[0]))] for col in range(len(grid))]
     state[0][0] = 1
@@ -44,31 +45,35 @@ def search(grid,init,goal,cost):
     cell_v = init[1]
     g = 0
     
-    current_state = [g, cell_h, cell_v]
+    current_state = [[g, cell_h, cell_v]]
     
-    while destination:
+    while destination and path:
         
         current_state.sort()
         current_state.reverse()
-        index = current_state.pop()
+        index_n = current_state.pop()
         
-        g = index[0]
-        cell_h = index[1]
-        cell_v = index[2]
+        g = index_n[0]
+        cell_h = index_n[1]
+        cell_v = index_n[2]
         
         if (cell_h == goal[0] and cell_v == goal[1]):
             destination = False
-    
+        
         else:
-            for i in delta:
-                cell_h_n = cell_h + i[0]
-                cell_v_n = cell_v + i[1]
-                if (cell_h_n >= 0 and cell_h_n < len(state)) and (cell_v_n >= 0 and cell_v_n < len(state[0])):
-                    if state[cell_h_n][cell_v_n] == 0 and grid[cell_h_n][cell_v_n] == 0:
-                        g_n = g + cost
-                        current_state.append([g_n, cell_h_n, cell_v_n])
-                        state[cell_h][cell_v] = 1
+            if len(current_state) == 0:
+                path = False
+                #print 'Destination not reachable'
+            else:
+                for i in delta:
+                    cell_h_n = cell_h + i[0]
+                    cell_v_n = cell_v + i[1]
+                    if (cell_h_n >= 0 and cell_h_n < len(state)) and (cell_v_n >= 0 and cell_v_n < len(state[0])):
+                        if state[cell_h_n][cell_v_n] == 0 and grid[cell_h_n][cell_v_n] == 0:
+                            g_n = g + cost
+                            current_state.append([g_n, cell_h_n, cell_v_n])
+                            state[cell_h_n][cell_v_n] = 1
             
-    return current_state
+    return index_n
 
 print search(grid,init,goal,cost)
