88547ee414b9130d167b705b5eabc604d98e307b
papillon1 <yasirhabib11@gmail.com>
2017-02-26 17:27:52 +0000

    Finished

--- Lesson 4 - Search/Print Path.py
+++ Lesson 4 - Search/Print Path.py
@@ -75,24 +75,6 @@ def search(grid,init,goal,cost):
                             g2 = g + cost
                             open.append([g2, x2, y2])
                             closed[x2][y2] = 1
-							
-							for i in range(len(delta)):
-                    x2 = x + delta[i][0]
-                    y2 = y + delta[i][1]
-                    if x2 >= 0 and x2 < len(grid) and y2 >=0 and y2 < len(grid[0]):
-                        if closed[x2][y2] == 0 and grid[x2][y2] == 0:
-                            g2 = g + cost
-                            open.append([g2, x2, y2])
-                            closed[x2][y2] = 1
-							
-							for i in range(len(delta)):
-                    x2 = x + delta[i][0]
-                    y2 = y + delta[i][1]
-                    if x2 >= 0 and x2 < len(grid) and y2 >=0 and y2 < len(grid[0]):
-                        if closed[x2][y2] == 0 and grid[x2][y2] == 0:
-                            g2 = g + cost
-                            open.append([g2, x2, y2])
-                            closed[x2][y2] = 1
                             expand[x2][y2] = '^'
 
     return expand # make sure you return the shortest path
