6b25b52a47ce5521c38d4ad6e6a2f5722daf8184
papillon1 <yasirhabib11@gmail.com>
2017-03-04 17:55:16 +0000

    Finished

--- Lesson 4 - Search/First Search Program.py
+++ Lesson 4 - Search/First Search Program.py
@@ -24,6 +24,7 @@ init = [0, 0]
 goal = [len(grid)-1, len(grid[0])-1]
 cost = 1
 
+
 delta = [[-1, 0], # go up
          [ 0,-1], # go left
          [ 1, 0], # go down
@@ -35,11 +36,8 @@ def search(grid,init,goal,cost):
     # ----------------------------------------
     # insert code here
     # ----------------------------------------
-    destination = True
-    path = True
-    
-    state = [[0 for row in range(len(grid[0]))] for col in range(len(grid))]
-    state[0][0] = 1
+    closed = [[0 for row in range(len(grid[0]))] for col in range(len(grid))]
+    closed[0][0] = 1
     
     cell_h = init[0]
     cell_v = init[1]
@@ -47,33 +45,41 @@ def search(grid,init,goal,cost):
     
     current_state = [[g, cell_h, cell_v]]
     
-    while destination and path:
-        
-        if (cell_h == goal[0] and cell_v == goal[1]):
-            destination = False
-        
-        else:
-            if len(current_state) == 0:
-                path = False
-                return 'fail'
-            else:
-                current_state.sort()
-                current_state.reverse()
-                index_n = current_state.pop()
-        
-                g = index_n[0]
-                cell_h = index_n[1]
-                cell_v = index_n[2]
-                
-                for i in delta:
-                    cell_h_n = cell_h + i[0]
-                    cell_v_n = cell_v + i[1]
-                    if (cell_h_n >= 0 and cell_h_n < len(state)) and (cell_v_n >= 0 and cell_v_n < len(state[0])):
-                        if state[cell_h_n][cell_v_n] == 0 and grid[cell_h_n][cell_v_n] == 0:
-                            g_n = g + cost
-                            current_state.append([g_n, cell_h_n, cell_v_n])
-                            state[cell_h_n][cell_v_n] = 1
+    destination = False
+    path = False
+    
+    while destination is False and path is False:
+        if len(current_state) == 0:
+            path = True
+            return 'fail'
             
+        else:
+            current_state.sort()
+            current_state.reverse()
+            index_n = current_state.pop()
+            
+            g = index_n[0]
+            cell_h = index_n[1]
+            cell_v = index_n[2]
+            
+        if (cell_h == goal[0] and cell_v == goal[1]):
+            destination = True
+            
+        else:
+            for i in range(len(delta)):
+                cell_h_n = cell_h + delta[i][0]
+                cell_v_n = cell_v + delta[i][1]
+                
+                if (cell_h_n >= 0 and cell_h_n < len(closed)) and (cell_v_n >= 0 and cell_v_n < len(closed[0])):
+                    if closed[cell_h_n][cell_v_n] == 0 and grid[cell_h_n][cell_v_n] == 0:
+                        g_n = g + cost
+                        current_state.append([g_n, cell_h_n, cell_v_n])
+                        closed[cell_h_n][cell_v_n] = 1
+                    
+            
+            
+            
+        
     return index_n
-
+    
 print search(grid,init,goal,cost)
