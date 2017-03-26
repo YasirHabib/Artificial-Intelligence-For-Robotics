8ec81e9478cf221ea6cd735621d3593c575383b0
papillon1 <yasirhabib11@gmail.com>
2017-03-26 09:59:11 +0100

    Finished

--- Lesson 4 - Search/Value Program.py
+++ Lesson 4 - Search/Value Program.py
@@ -14,6 +14,15 @@ grid = [[0, 1, 0, 0, 0, 0],
         [0, 1, 0, 0, 0, 0],
         [0, 1, 0, 0, 0, 0],
         [0, 0, 0, 0, 1, 0]]
+
+# grid = [[0, 0, 1, 0, 0, 0],
+#         [0, 0, 1, 0, 0, 0],
+#         [0, 0, 1, 0, 0, 0],
+#         [0, 0, 0, 0, 1, 0],
+#         [0, 0, 1, 1, 1, 0],
+#         [0, 0, 0, 0, 1, 0]]
+        
+        
 goal = [len(grid)-1, len(grid[0])-1]
 cost = 1 # the cost associated with moving from a cell to an adjacent one
 
@@ -24,11 +33,48 @@ delta = [[-1, 0 ], # go up
 
 delta_name = ['^', '<', 'v', '>']
 
+
+#pos = [0, goal[0], goal[1]]
+
+Goal = True
+
 def compute_value(grid,goal,cost):
     # ----------------------------------------
     # insert code below
     # ----------------------------------------
     
+    value = [[99 for j in grid[0]] for i in grid]
+    open = []
+    open.append([0, goal[0], goal[1]])
+    value[goal[0]][goal[1]] = 0
+    
+    while(Goal):
+        if (len(open) == 0):
+            break
+        else:
+            open.sort()
+            open.reverse()
+            pos = open.pop()
+            for i in range(len(delta)):
+                x = pos[1]
+                y = pos[2]
+                g = pos[0]
+                x2 = x + delta[i][0]
+                y2 = y + delta[i][1]
+                if x2 >= 0 and x2 < len(grid) and y2 >=0 and y2 < len(grid[0]):
+                    if value[x2][y2] == 99 and grid[x2][y2] == 0:
+                        value[x2][y2] = value[x][y] + cost
+                        g2 = g + 1 
+                        open.append([g2, x2, y2])                    
+   
+
+    
+    
     # make sure your function returns a grid of values as 
     # demonstrated in the previous video.
     return value 
+
+result = compute_value(grid,goal,cost)
+
+for i in range(len(result)):
+    print result[i]
