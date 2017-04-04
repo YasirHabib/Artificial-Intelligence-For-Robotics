af8e6ed314a5e17aa770a28662e4a16afa89ba87
papillon1 <yasirhabib11@gmail.com>
2017-04-04 21:25:14 +0100

    Finished

--- Lesson 5 - PID Control/Constrained Smoothing.py
+++ Lesson 5 - PID Control/Constrained Smoothing.py
@@ -17,12 +17,26 @@
 #
 
 ######################## ENTER CODE BELOW HERE #########################
+from copy import deepcopy
 
 def smooth(path, fix, weight_data = 0.0, weight_smooth = 0.1, tolerance = 0.00001):
     #
     # Enter code here. 
     # The weight for each of the two new equations should be 0.5 * weight_smooth
     #
+    newpath = deepcopy(path)
+    
+    total_change = tolerance
+    while total_change >= tolerance:
+        total_change = 0.0
+        for i in range(len(path)):
+            for j in range(len(path[0])):
+                initial_value = newpath[i][j]
+                newpath[i][j] += weight_smooth * (newpath[(i+1) % len(path)][j] + newpath[(i-1) % len(path)][j] - 2.0 * newpath[i][j]) + \
+                                 0.5 * weight_smooth * (2 * (newpath[(i-1) % len(path)][j]) - newpath[(i-2) % len(path)][j] - newpath[i][j]) + \
+                                 0.5 * weight_smooth * (2 * (newpath[(i+1) % len(path)][j]) - newpath[(i+2) % len(path)][j] - newpath[i][j])
+                
+                total_change += abs(newpath[i][j] - initial_value)
     return newpath
 
 # --------------
